7715d0f0d413f63c6198de3eaeea1a7033d9bb9f
Alex Noir <Varsash@gmail.com>
2022-11-11 01:16:58 +0300

    Add "next" button and make the current text test wait on user input Tweak screenshake slightly

--- Main.gd
+++ Main.gd
@@ -11,6 +11,7 @@ var current_char
 # animation to play after text is done
 var post_anim = ""
 
+
 func _on_ButtonTest_pressed():
 	# Reset the background to be visible, hide the zoom
 	$World/Background.set_visible(true)
@@ -43,18 +44,21 @@ func _on_ButtonTest_pressed():
 	$SFXPlayer.play()
 	dialog.add_text(" AND dad!")
 	dialog.display_text()
+
+	yield(dialog, "text_displayed")
+	$HUD/NextButton.set_disabled(false)
+	yield($HUD/NextButton, "pressed")
+	$HUD/NextButton.set_disabled(true)
 	
 	# Hide dialog box, slam the desk, make Phoenix talk with his hands still on desk, screenflash
-	yield(get_tree().create_timer(1.25), "timeout")
 	dialog.set_visible(false)
 	current_char.play("deskslam")
 	yield(get_tree().create_timer(0.2), "timeout")
 	# Screenshake
-	$World/Camera2D.add_trauma(0.4)
+	$World/Camera2D.add_trauma(0.5)
 	yield(get_tree().create_timer(1.25), "timeout")
 	post_anim = "handsondesk(a)"
 	current_char.play("handsondesk(b)")
-	
 	# Play the screenflash animation and sound effect
 	$SFXPlayer.set_stream(load("res://res/Sounds/realization.wav"))
 	$SFXPlayer.play()
@@ -62,9 +66,13 @@ func _on_ButtonTest_pressed():
 	dialog.set_visible(true)
 	dialog.set_text("WHAT the [color=lime]fuck[/color] do you MEAN!?")
 	dialog.display_text()
+
+	yield(dialog, "text_displayed")
+	$HUD/NextButton.set_disabled(false)
+	yield($HUD/NextButton, "pressed")
+	$HUD/NextButton.set_disabled(true)
 	
 	# Screen Panning Test
-	yield(get_tree().create_timer(1.5), "timeout")
 	$World/CameraAnimPlayer.play("def_to_pro")
 	dialog.display_text()
 	dialog.set_text("[shake]WOAAAAAAAHHHHHHHHHH[/shake]")
@@ -72,18 +80,19 @@ func _on_ButtonTest_pressed():
 	$World/CameraAnimPlayer.play("pro_to_def")
 	dialog.display_text()
 	dialog.set_text("Back to me baby")
-	yield(get_tree().create_timer(1.25), "timeout")
+
+	yield(dialog, "text_displayed")
+	$HUD/NextButton.set_disabled(false)
+	yield($HUD/NextButton, "pressed")
+	$HUD/NextButton.set_disabled(true)
 	
 	# Objection bubble
 	$SFXPlayer.set_stream(load("res://res/Sounds/objection.wav"))
 	$SFXPlayer.play()
 	$HUD/Bubble/AnimationPlayer.play("objection")
-	
 	# Stop music
 	$MusicPlayer.stop_music(1)
-	
 	yield(get_tree().create_timer(0.7), "timeout")
-	
 	# Swap pos to judge, make judge fart
 	$World/CameraAnimPlayer.play("jud")
 	current_char = $World/Characters/Judge/AnimationPlayer
@@ -93,7 +102,10 @@ func _on_ButtonTest_pressed():
 	dialog.set_text("I [color=#FF773D]farted[/color], [wave]lmao!![/wave]")
 	dialog.display_text()
 	
-	yield(get_tree().create_timer(1.25), "timeout")
+	yield(dialog, "text_displayed")
+	$HUD/NextButton.set_disabled(false)
+	yield($HUD/NextButton, "pressed")
+	$HUD/NextButton.set_disabled(true)
 
 	# Phoenix Objection
 	$SFXPlayer.set_stream(load("res://res/Sounds/Voice/Phoenix - objection.wav"))
@@ -116,7 +128,10 @@ func _on_ButtonTest_pressed():
 	dialog.set_text("Your honor, this paper proves a very large contradiction.")
 	dialog.display_text()
 
-	yield(get_tree().create_timer(2.6), "timeout")
+	yield(dialog, "text_displayed")
+	$HUD/NextButton.set_disabled(false)
+	yield($HUD/NextButton, "pressed")
+	$HUD/NextButton.set_disabled(true)
 
 	# Phoenix face zoom!!
 	$World/Background.set_visible(false)
@@ -125,7 +140,7 @@ func _on_ButtonTest_pressed():
 	$SFXPlayer.set_stream(load("res://res/Sounds/objection.wav"))
 	$SFXPlayer.play()
 	# Screenshake
-	$World/Camera2D.add_trauma(0.4)
+	$World/Camera2D.add_trauma(0.5)
 	current_char.play("zoom(b)")
 	post_anim = "zoom(a)"
 	dialog.set_text("You [color=#FF773D]can't physically fart[/color]!")
